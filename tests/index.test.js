8b061568fcbb8dc7c8433e19fdaabba41ed9fde0
DUBOIS Arthur <arthur.dbs70@gmail.com>
2024-06-20 12:55:45 +0000

    remove db test

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,13 +3,6 @@ const app = require('../server');
 
 
 describe('Test the users API', () => {
-    it('should add a new user', async () => {
-        const response = await request(app)
-            .post('/adduser')
-            .send({ name: 'Test User', point: 5 });
-        expect(response.statusCode).toBe(200);
-        expect(response.text).toBe('User added...');
-    });
 
     it('GET /hello should return Hello Jest', async () => {
         const response = await request(app).get('/hello');
